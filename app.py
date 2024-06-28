53b93928fe1c96d900e9aa148d52ccba240ce2b9
Devarsh-42 <devarshmehta2005@gmail.com>
2024-06-28 16:45:14 +0530

    New Commit

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a website built using flask, YEEEHHH!!'
+    return render_template('home.html', jobs=JOBS, company_name='Wayne Enterprises')
 
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs.10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs.20,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Data Engineer',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs.30,00,000'
+    },
+    {
+        'id': 4,
+        'title': 'Software Engineer',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs.5,00,000'
+    },
+]
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8080)
